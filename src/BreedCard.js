9d2a3b6e0635f52cce0a56c6f51b1fb292aa92e8
EstrellitaSitez <etrellita.brillosa.11@gmail.com>
2022-02-24 20:12:58 -0400

    urls updated

--- src/BreedCard.js
+++ src/BreedCard.js
@@ -9,41 +9,51 @@ import {LinkedinShareButton, LinkedinIcon, WhatsappShareButton, WhatsappIcon} fr
 
 
 export default function BreedCard(props){
+    let baseUrl = 'https://estrellitasitez.github.io/cat-app/breed/'
     let breed = props.breed
     console.log(breed)
     return(
+     <>
+       {breed != null?
         <Card sx={{ maxWidth: 400, maxHeight: 'fit-content' }}>
-      <CardMedia
-        component="img"
-        height="300"
-        image={breed.image.url}
-        alt={breed.name}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-            {breed.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-            {breed.description}
-        </Typography>
-      </CardContent>
-      <CardActions style={{display: 'flex', justifyContent: 'center'}}>
-        {/* <Button size="small">Share</Button> */}
-            <h5 style={{color:'grey', marginRight:'2%'}}>
-                Share
-            </h5>
-            <span >
-           <LinkedinShareButton url = {'pie.com'} style={{marginRight:'1%'}}>
-            <LinkedinIcon size={32}/>
-            </LinkedinShareButton>
-
-            <WhatsappShareButton url = {'pie.com'} title={`Hey check out this cat. It's breed is ${breed.name}`} >
-                <WhatsappIcon size={32}/>
-            </WhatsappShareButton>
-            </span>
-            
-        <Button target="_blank" href={breed.wikipedia_url} size='small'>Learn More</Button>
-      </CardActions>
-    </Card>
+        <CardMedia
+          component="img"
+          height="300"
+          image={breed.image.url}
+          alt={breed.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+              {breed.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+              {breed.description}
+          </Typography>
+        </CardContent>
+        <CardActions style={{display: 'flex', justifyContent: 'center'}}>
+          {/* <Button size="small">Share</Button> */}
+              <h5 style={{color:'grey', marginRight:'2%'}}>
+                  Share
+              </h5>
+              <span >
+             <LinkedinShareButton url = {baseUrl+(breed.name)} style={{marginRight:'1%'}}>
+              <LinkedinIcon size={32}/>
+              </LinkedinShareButton>
+  
+              <WhatsappShareButton url = {baseUrl+(breed.name)} title={`Hey check out this cat. It's breed is ${breed.name}`} >
+                  <WhatsappIcon size={32}/>
+              </WhatsappShareButton>
+              </span>
+              
+          <Button target="_blank" href={breed.wikipedia_url} size='small'>Learn More</Button>
+        </CardActions>
+      </Card>
+      :
+      <div>
+        Looks like you have not chosen a breed
+      </div>
+      
+      }
+     </>
     )
 }
